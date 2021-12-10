beb546a921c812a3c474262ebf53b59c6e981935
Shaik Nazeeruddin <nazeeruddin.shaik@oracle.com>
2021-12-10 13:23:48 +0530

    deploy to heroku

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,23 @@
+from _pytest.python_api import raises
+import pytest
 
+class NotInRange(Exception):
+    def __init__(self, message="not in range"):
+        self.message=message
+        super().__init__(self.message)
+
+def test_execption():
+    test = 5
+    #test = 11
+    with pytest.raises(NotInRange):
+        if test not in range(10,20):
+            raise NotInRange
+
+# def test_exception() :
+#     a = 1
+#     with pytest.raises(ValueError) :
+#         if a not in range(10, 20) :
+#             raise NotInRange
 
 def test_generic() :
     a = 2
